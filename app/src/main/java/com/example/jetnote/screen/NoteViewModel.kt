e36d461395bc123b0531f8a27db1af87bb343ce2
pavlos <pavlos.repin@gmail.com>
2024-08-13 20:52:26 +0300

    Can connect have to improve on Timeout

--- app/src/main/java/com/example/jetnote/screen/NoteViewModel.kt
+++ app/src/main/java/com/example/jetnote/screen/NoteViewModel.kt
@@ -20,6 +20,7 @@ import okhttp3.RequestBody.Companion.toRequestBody
 import java.io.File
 import java.io.FileOutputStream
 import java.io.IOException
+import java.util.concurrent.TimeUnit
 import javax.inject.Inject
 import kotlin.coroutines.CoroutineContext
 
@@ -28,11 +29,10 @@ class NoteViewModel @Inject constructor(private val repository: NoteRepository):
     private var _noteList = MutableStateFlow<List<Note>>(emptyList())
     val noteList = _noteList.asStateFlow()
 
-    private val client = OkHttpClient()
-    val MEDIA_TYPE_MARKDOWN = "text/x-markdown; charset=utf-8".toMediaType()
+    var client = OkHttpClient()
     val folder: File =
         Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS)
-    val file = File(folder, "geeksData.txt")
+    var file = File(folder, "geeksData.txt")
 
 
     init {
@@ -53,13 +53,8 @@ class NoteViewModel @Inject constructor(private val repository: NoteRepository):
      fun addNote(note: Note) = viewModelScope.launch{
          repository.addNote(note)
 
-         // getExternalStoragePublicDirectory() represents root of external storage, we are using DOWNLOADS
-         // We can use following directories: MUSIC, PODCASTS, ALARMS, RINGTONES, NOTIFICATIONS, PICTURES, MOVIES
-         val folder: File =
-             Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS)
-
          // Storing the data in file with name as geeksData.txt
-         val file = File(folder, "geeksData.txt")
+         file = File(folder, "geeksData.txt")
          writeTextData(file, note.description, context = currentCoroutineContext())
          // displaying a toast message
      }
@@ -98,11 +93,17 @@ class NoteViewModel @Inject constructor(private val repository: NoteRepository):
 //                .post(formBody)
 //                .build()
 
-            val file = File("geeksData.txt")
+            //val file = File("geeksData.txt")
+
+            val postBody = _noteList.value.last().description
+
+            val formBody = FormBody.Builder()
+                .add("test", postBody)
+                .build()
 
             val request = Request.Builder()
-                .url("http://10.101.176.40:5000/test")
-                .post(file.asRequestBody(MEDIA_TYPE_MARKDOWN))
+                .url("http://10.101.176.109:5000/test")
+                .post(formBody)
                 .build()
 
             client.newCall(request).execute().use { response ->
